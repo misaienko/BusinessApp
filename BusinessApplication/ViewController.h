3701b33c1a180a84cbb8eb8e2bebd116ab22b787
Maryna Isaienko <macprobp@gmail.com>
2023-06-09 16:05:04 -0700

    Initial Commit

---
+++ BusinessApplication/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BusinessApplication
+//
+//  Created by Admin on 09/06/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
